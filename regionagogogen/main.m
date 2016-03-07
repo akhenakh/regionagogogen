895db9d5767f257bcced2bee98b199e23e6ace43
Fabrice Aneche <akh@nobugware.com>
2016-03-07 11:34:48 -0500

    Initial Commit

---
+++ regionagogogen/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  regionagogogen
+//
+//  Created by AkH on 3/7/16.
+//  Copyright © 2016 Nobugware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
